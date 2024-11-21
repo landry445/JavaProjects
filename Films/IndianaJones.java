4d75384204c18cd18f8b1e51305c75f886e79f68
Landry Dupont <landrydupont790@gmail.com>
2024-11-21 19:19:48 +0100

    Variables

---
+++ Films/IndianaJones.java
@@ -0,0 +1,16 @@
+package Films;
+
+public class IndianaJones {
+
+    public static void main(String[] args) {
+        String film = "Indiana Jones et la Dernière Croisade";
+        Boolean iSawTheFilm = true;
+        int year = 1989;
+        float rating = 8.2f;
+
+        System.out.println("Film: " + film);
+        System.out.println("Have I seen it? " + iSawTheFilm);
+        System.out.println("Release year: " + year);
+        System.out.println("Rating: " + rating + "/10");
+    }
+}
