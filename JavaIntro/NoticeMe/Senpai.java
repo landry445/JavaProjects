0b3d9e2cb19f2c174a37a499e6624210a19e679b
Landry Dupont <landrydupont790@gmail.com>
2024-11-21 13:32:42 +0100

    Le JDK

---
+++ JavaIntro/NoticeMe/Senpai.java
@@ -0,0 +1,8 @@
+package NoticeMe;
+
+public class Senpai {
+    public static void main(String[] args) {
+        // afficher Hello World! dans le terminal
+        System.out.println("Notice me Senpai");
+    }
+}
